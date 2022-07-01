5add5937358fbb8b956748113e16e4bc768e6fc8
khier bouadam <66218152+khierCr7@users.noreply.github.com>
2022-07-01 19:51:40 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,10 @@
                         </g>
                     </svg>
                 </div>
-                <h1 style="text-align: center; color: #fff ;">HELLO I'M KHIER BOUADAM</h1>
+                <h1 style="text-align: center; color: #fff ;">
+                     HELLO I'M KHIER BOUADAM <br>
+                     On Production
+                </h1>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
